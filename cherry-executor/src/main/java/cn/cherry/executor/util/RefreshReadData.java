72ca7c5265a6aebf10a0a46a7a6c1238dcfce886
sinbad.cheng <sinbad.cheng@jtexpress.com>
2023-09-12 17:52:55 +0800

    feat:

---
+++ cherry-executor/src/main/java/cn/cherry/executor/util/RefreshReadData.java
@@ -0,0 +1,38 @@
+package cn.cherry.executor.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author: sinbad.cheng
+ * @time: 2023/9/11 16:43
+ * @description：
+ */
+public class RefreshReadData {
+
+    public static void main(String[] args) {
+        // 创建 ObjectMapper 对象，用于读取 JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // JSON 文件路径
+        String jsonFilePath = "src/main/java/cn/cherry/executor/util/0911Waybill.json";
+
+        try {
+            // 读取 JSON 文件
+            File jsonFile = new File(jsonFilePath);
+
+            List<String> waybills=objectMapper.readValue(jsonFile, new TypeReference<List<String>>() {});
+
+            System.out.println(waybills);
+            System.out.println(waybills.size());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
